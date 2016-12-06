66243bbfed6f2f1fae477d941702b678d308ab47
DembitskyiBohdan <bodik_net@gmail.com>
2016-12-06 19:37:53 +0200

    Commit3

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -5,5 +5,10 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Введіть своє ім'я:");
 		System.out.println("Hello, " + sc.next() + "!");
+		System.out.println("Введіть свій рівень доступу");
+		if(sc.next().equals("admin")){
+			System.out.println("Ви ввійшли як адміністратор");
+		}
+		else System.out.println("У вас права доступу звичайного користувача");
 	}
 }
